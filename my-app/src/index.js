89015c051af9853a8625e94234ccf3cfb00a426b
Nicolas Battaglia <nicobattmdq@gmail.com>
2023-02-05 23:02:58 -0300

    Add x to game

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,22 +3,35 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 class Square extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            value:null,
+        }
+    }
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button className="square"
+         onClick={()=> {this.setState({value: 'X'})}}>
+          {this.state.value}
         </button>
       );
     }
   }
   
   class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null)
+        }
+    }
     renderSquare(i) {
-      return <Square />;
+      return <Square value={this.state.squares[i]}/>;
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: Empty';
   
       return (
         <div>
